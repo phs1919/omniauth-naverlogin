8408d25e0803f3a47853e4fe5b15c9d656964068
Richard Park <phs1919@gmail.com>
2015-07-21 15:30:30 +0900

    Add naverlogin_spec with basic client options tests

---
+++ spec/omniauth/strategies/naverlogin_spec.rb
@@ -0,0 +1,19 @@
+describe OmniAuth::Strategies::NaverLogin do
+  subject do
+    OmniAuth::Strategies::NaverLogin.new({})
+  end
+
+  describe "client options" do    
+    it "has correct site option" do
+      expect(subject.options.client_options.site).to eq("https://nid.naver.com")
+    end
+    
+    it "has correct authorize_url" do
+      expect(subject.options.client_options.authorize_url).to eq("https://nid.naver.com/oauth2.0/authorize")
+    end
+    
+    it "has correct token_url" do
+      expect(subject.options.client_options.token_url).to eq("https://nid.naver.com/oauth2.0/token")
+    end
+  end
+end
